f6a58bec32bb59d6e4e72ac418924b7219f0447f
Tommy Barth <tommyhbrc@gmail.com>
2014-10-19 11:04:06 -0400

    Completes feedback report

--- app/js/router.js
+++ app/js/router.js
@@ -1,10 +1,19 @@
 Feedback.Router.map(function() {
   this.route('enterFeedback', {route: '/enterFeedback'});
   this.route('clientsView', {path: '/clientsView'});
+  this.resource('mentors', function() {
+    this.route('view', {path: ':report_id'});
+  });
 });
 
 Feedback.ClientsViewRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('report');
+  }
+});
+
+Feedback.ReportsViewRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('report', params.goal_id);
   }
 });
